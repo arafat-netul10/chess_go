1ed1e12ac1bf0504825075b6073664207b9f2b60
Netul <arafatnetul10@gmail.com>
2026-09-19 22:53:59 +0600

    Add short vercel build script file

---
+++ vercel-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+./flutter/bin/flutter build web --release --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
